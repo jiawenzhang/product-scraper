50d9fbc2347e0c30f740ad336d0b7375a867e1ac
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2016-08-19 00:13:18 -0400

    Add scraper test cases

--- test/test.js
+++ test/test.js
@@ -1,99 +1,59 @@
-var assert = require('assert'),
-	scraper = require('../index');
+var assert = require('assert');
+var scraper = require('../index');
 
-Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
+require('it-each')();
+
+const title = "title";
+const description = "description";
+const price = "price";
+const image = "image";
+const images = "images";
+
+const tests = [
+    {
+        store: "fancy",
+        url: "https://fancy.com/things/1224643338880157789/Women%27s-1960%27s-Retro-Oversize-Round-Sunglasses-A259?utm=newest",
+        expected: [title, description]
+    },
+    {
+        store: "ebay",
+        url: "http://deals.ebay.ca/5003428771_Apple_11_6__MacBook_Air_MD223LL_A_i5_4GB_Memory_64GB_Flash_Storage?_trksid=p2050601.m1256&_trkparms=algo%3DDailyDeals%26clkid%3D7594163878637097118",
+        expected: [title, description, price, images]
+    },
+    {
+        store: "forever21",
+        url: "http://www.forever21.com/CA/Product/Product.aspx?br=LOVE21&category=dress&productid=2000237537",
+        expected: [title, description, price, image]
+    },
+    {
+        store: "victoriassecret",
+        url: "https://www.victoriassecret.com/swimwear/all-sale-and-clearance/the-crochet-surf-fabulous-top?ProductID=280377&CatalogueType=OLS",
+        expected: [title, description, image]
+    },
+    {
+        store: "walmart",
+        url: "http://www.walmart.ca/en/ip/16-toro-wheel-cover-4-pack/6000069712155",
+        expected: [title, description, price, image]
     }
-    return size;
-};
+];
 
-describe('stores', function(){
 
-	describe('amazon', function(){
-		this.slow(6500);
-
-		it('has not changed markup', function(done){
-			this.timeout(10000);
-
-			scraper.init('http://www.amazon.com/gp/product/B00X4WHP5E/', function(data){
-				
-				if(Object.size(data) != 7) throw err;
-				done();
-
-			});
-
-		});
-
-	});
-
-	describe('bestbuy', function(){
-		this.slow(6500);
-
-		it('has not changed markup', function(done){
-			this.timeout(10000);
-
-			scraper.init('http://www.bestbuy.com/site/apple-macbook-pro-15-4-display-intel-core-i7-16gb-memory-256gb-flash-storage-silver/8532502.p?id=1219343246944&skuId=8532502', function(data){
-				
-				if(Object.size(data) != 6) throw err;
-				done();
-
-			});
-
-		});
-
-	});
-
-	describe('etsy', function(){
-		this.slow(6500);
-
-		it('has not changed markup', function(done){
-			this.timeout(10000);
-
-			scraper.init('https://www.etsy.com/listing/255992683/iskelter-case-built-for-iphone-66s-and', function(data){
-				
-				if(Object.size(data) != 6) throw err;
-				done();
-
-			});
-
-		});
-
-	});
-
-	describe('target', function(){
-		this.slow(6500);
-
-		it('has not changed markup', function(done){
-			this.timeout(10000);
-
-			scraper.init('http://www.target.com/p/canon-eos-rebel-sl1-18mp-digital-slr-camera-with-18-55mm-lens/-/A-14537289?lnk=rec|pdp|top_rated|cath770X250', function(data){
-				
-				if(Object.size(data) != 5) throw err;
-				done();
-
-			});
-
-		});
-
-	});
-
-	describe('walmart', function(){
-		this.slow(6500);
-
-		it('has not changed markup', function(done){
-			this.timeout(10000);
-
-			scraper.init('http://www.walmart.com/ip/40549455', function(data){
-				
-				if(Object.size(data) != 6) throw err;
-				done();
-
-			});
-
-		});
-
-	});
-
+describe('Testing scrapering urls', function() {
+    it.each(tests, 'loads', function(test, next) {
+        this.timeout(20000);
+        console.log(test.store);
+        scraper.init(test.url, function(data) {
+            for (var key in test.expected) {
+                if (test.expected.hasOwnProperty(key)) {
+                    var property = test.expected[key];
+                    if (!data.hasOwnProperty(property)) {
+                        console.log("missing " + property);
+                        console.log(JSON.stringify(data));
+                        throw err;
+                    }
+                }
+            }
+            next();
+        });
+    });
 });
