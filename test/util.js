58239dbafbcc67722b90c6fba38da8ce6eb7d0d5
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2016-08-20 16:04:05 -0400

    Support extracting currency symbol from string

--- test/util.js
+++ test/util.js
@@ -5,13 +5,23 @@ describe('util', function(){
 
     describe('extractCurrencySymbol', function() {
         it('valid', function(done) {
-          // C $249.99 -> CAD $
-          // $49.97 -> $
-          // CAD $33.90 -> CAD $
+            // C $249.99 -> CAD $ // how to know is CAD?
             assert.equal(util.extractCurrencySymbol("199 CAD"), "CAD");
             assert.equal(util.extractCurrencySymbol("CAD $33.90"), "CAD");
             assert.equal(util.extractCurrencySymbol("199 USD"), "USD");
             assert.equal(util.extractCurrencySymbol("USD $33.90"), "USD");
+            assert.equal(util.extractCurrencySymbol("€ EUR 33.90"), "EUR");
+
+            assert.equal(util.extractCurrencySymbol("$ 33.90"), "$"); // could be USD, CAD or many others, how to distinguish?
+            assert.equal(util.extractCurrencySymbol("¥ 33.90"), "¥");
+            assert.equal(util.extractCurrencySymbol("$ 33.90 ¥"), "$");
+            assert.equal(util.extractCurrencySymbol("€33.90"), "€");
+            assert.equal(util.extractCurrencySymbol("¥  33.90 "), "¥"); // could be CNY or JPY, how to distinguish?
+            assert.equal(util.extractCurrencySymbol("S/.199 "), "S/."); //http://www.xe.com/currency/pen-peruvian-sol
+            assert.equal(util.extractCurrencySymbol("C$100"), "C$"); //http://www.xe.com/currency/nio-nicaraguan-cordoba
+            assert.equal(util.extractCurrencySymbol("Дин.100"), "Дин."); //
+            // assert.equal(util.extractCurrencySymbol("РСД50"), "РСД"); //http://www.xe.com/currency/rsd-serbian-dinar, fail!
+
             done();
         });
     });
@@ -19,6 +29,10 @@ describe('util', function(){
 
     describe('extractFloat', function(){
         it('valid', function(done){
+          /** Fixme: unsupported currency
+          http://www.xe.com/currency/pen-peruvian-sol example S/.200 , where S/. is the currency symbol
+          **/
+
             assert.equal(util.extractFloat("I have 1 pound"), 1);
             assert.equal(util.extractFloat("I have £3.50 to spend"), 3.50);
             assert.equal(util.extractFloat("I have 23.00 pounds"), 23.00);
--- util.js
+++ util.js
@@ -1,4 +1,7 @@
+// there are 179 codes in cc
+// and there are 117 codes in currencySymbolMap
 var cc = require('./local_modules/currency-codes');
+var currencySymbolMap = require('currency-symbol-map').currencySymbolMap;
 
 exports.extractFloat = function(str) {
     // see http://stackoverflow.com/questions/17885850/how-to-parse-a-string-containing-text-for-a-number-float-in-javascript
@@ -8,27 +11,53 @@ exports.extractFloat = function(str) {
     return matches && matches[0] || null;
 }
 
-var regex;
+var regexCode;
+var regexSymbol;
 
 exports.extractCurrencySymbol = function(str) {
     // Match currency code defined in ISO 4247 (USD, CAD ...)
-    // Fixme: also need to match currency symbol like $
-    if (!regex) {
-      console.log("no regex");
-      const codes = cc.codes();
-      // construct a regex like "(CAD|USD|...)"
+    // prefer currency code, as it is ISO standarded
+    if (!regexCode) {
+      // construct a regex like "(CAD|USD|EUR|...)"
       var regexStr = "(";
-      for (var i=0; i< codes.length; i++) {
-        regexStr = regexStr + codes[i] + "|"
+      for (var code in currencySymbolMap) {
+        regexStr = regexStr + code + "|"
       }
 
       // cut the last "|"
       regexStr = regexStr.slice(0, -1);
       regexStr = regexStr + ")";
 
-      regex = new RegExp(regexStr, "g");
+      regexCode = new RegExp(regexStr, "g");
     }
 
-    var matches = str.match(regex);
-    return matches && matches[0] || null;
+    var codes = str.match(regexCode);
+    if (codes && codes[0]) {
+      return codes[0];
+    }
+
+    // there there is no match to ISO currency code, try to match currency symbol
+    if (!regexSymbol) {
+      // construct a regex like "($|£|€|...)"
+      var regexStr = "(";
+      for (var code in currencySymbolMap) {
+        var symbol = currencySymbolMap[code];
+        // $ and . are special characters in regex, need to excape them
+        symbol = symbol.replace(/\$/g, '\\$');
+        symbol = symbol.replace(/\./g, '\\.');
+        regexStr = regexStr + symbol + "|";
+      }
+
+      // cut the last "|"
+      regexStr = regexStr.slice(0, -1);
+      regexStr = regexStr + ")";
+
+      regexSymbol = new RegExp(regexStr, "g");
+    }
+
+    var symbols = str.match(regexSymbol);
+    if (symbols && symbols[0]) {
+      return symbols[0];
+    }
+    return null;
 }
